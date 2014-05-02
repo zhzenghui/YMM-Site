9cf0e44029fdd86ebb2d9ea5b1587bd7f4af4c18
zhzenghui <zhzenghui@gmail.com>
2014-05-03 00:28:07 +0800

    production  precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Yue::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
