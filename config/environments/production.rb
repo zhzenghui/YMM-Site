bc413fea1bbc496b4995c232e849c4ff63d81922
zhzenghui <zhzenghui@gmail.com>
2014-05-03 23:33:36 +0800

    解决 production  不显示图片的问题

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,23 @@ Yue::Application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin"
 
 
+ # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :scss
+
+  config.serve_static_assets = true
+  # Compress JavaScripts and CSS
+  config.assets.compress = true 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false 
+  # Generate digests for assets URLs
+  config.assets.digest = true 
+  config.assets.enabled = true
 
 
 end
