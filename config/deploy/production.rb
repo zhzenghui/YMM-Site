c6faa8b9d1f4db7b4aa536bb03ea2b65b2dd1573
zhzenghui <zhzenghui@gmail.com>
2014-05-05 16:12:37 +0800

    update  unicorn_config, unicorn_pid     path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -37,8 +37,8 @@ set :nginx_pid, "/var/run/nginx.pid"
 
 
 set :unicorn_service, "unicorn_#{fetch(:application)}_#{fetch(:stage)}"
-set :unicorn_pid, "#{fetch(:deploy_to)}/tmp/pids/unicorn.pid"
-set :unicorn_config, "#{fetch(:deploy_to)}/config/unicorn.rb"
+set :unicorn_pid, "#{fetch(:deploy_to)}/current/tmp/pids/unicorn.pid"
+set :unicorn_config, "#{fetch(:deploy_to)}/current/config/unicorn.rb"
 set :unicorn_workers, 1
 
 
