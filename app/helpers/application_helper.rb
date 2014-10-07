6cb413d20124c8d30e724f292cdd646c0cf795e0
zhzenghui <zhzenghui@gmail.com>
2014-10-07 22:45:38 +0800

    修改网站名称

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Plan App"
+    base_title = "自由世界"
     if page_title.empty?
       base_title
     else
