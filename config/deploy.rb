992d062f14413b544d747e01d8495892b902d173
zhzenghui <zhzenghui@gmail.com>
2014-10-25 00:04:51 +0800

    add gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,15 +71,6 @@ namespace :deploy do
     end
   end
 
-  task :add_default_hooks do
-    after 'deploy:starting', 'sidekiq:quiet'
-    after 'deploy:updated', 'sidekiq:stop'
-    after 'deploy:reverted', 'sidekiq:stop'
-    after 'deploy:published', 'sidekiq:start'
-  end
-
-
-
 
   after :finishing, "deploy:cleanup"
 
